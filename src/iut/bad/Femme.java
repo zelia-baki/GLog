2ba660f2ed171e14ae97e29089a513f1d56ee3c6
zelia-baki <rakotomalala.babazhelia@gmail.com>
2025-08-22 06:53:14 +0300

    E13: Dans la classe Femme, creation d'ne methode main() pour tester leur amitie

--- src/iut/bad/Femme.java
+++ src/iut/bad/Femme.java
@@ -10,7 +10,18 @@ public class Femme extends humain  {
 	public Femme(String nom, String prenom, int age) {
 		super(nom, prenom, age);
 		//TODO Auto-generated constructor stub
-	}   
-	    
+	} 
+	
+	 public static void main(String[] args) {
+	        Homme h = new Homme("TSIMIJALY", "Nomena", 18);
+	        Femme f = new Femme("Gaby", "Montez", 22);
+
+	        h.details();
+	        f.details();
+	        
+	        System.out.println("\nDéclaration d'amitié :");
+	        h.amis(f);   // TSIMIJALY est ami avec Gaby
+	        f.amis(h);   // Gaby est amie avec TSIMIJALY
+	 }
 	
 }
